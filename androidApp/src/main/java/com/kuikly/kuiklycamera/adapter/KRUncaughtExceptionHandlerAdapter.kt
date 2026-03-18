522eb7ccc991f051ec9dce0f3527dfead421aba9
yukiykchen <yukiykchen@tencent.com>
2026-03-18 15:00:24 +0800

    init

---
+++ androidApp/src/main/java/com/kuikly/kuiklycamera/adapter/KRUncaughtExceptionHandlerAdapter.kt
@@ -0,0 +1,19 @@
+package com.kuikly.kuiklycamera.adapter
+
+import android.util.Log
+import com.tencent.kuikly.core.render.android.adapter.IKRUncaughtExceptionHandlerAdapter
+import com.kuikly.kuiklycamera.BuildConfig
+
+object KRUncaughtExceptionHandlerAdapter : IKRUncaughtExceptionHandlerAdapter {
+
+    private const val TAG = "KRExceptionHandler"
+
+    override fun uncaughtException(throwable: Throwable) {
+        if (BuildConfig.DEBUG) {
+            throw throwable
+        } else {
+            Log.e(TAG, "KR error: ${throwable.stackTraceToString()}")
+        }
+    }
+
+}
